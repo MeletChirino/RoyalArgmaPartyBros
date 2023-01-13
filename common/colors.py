c18319ec8765aedef5088dbb03c8d73308826176
MeletChirino <melet.chirino@gmail.com>
2023-01-13 23:40:45 +0100

    Board shown on pygame canvas

---
+++ common/colors.py
@@ -0,0 +1,5 @@
+class Color:
+    def __init__(self):
+        self.WHITE = (255, 255, 255)
+        self.BLACK = (  0,   0,   0)
+        self.RED =   (255,   0,   0)
--- run.py
+++ run.py
@@ -1,5 +1,8 @@
 import pygame, sys
 from game_elements.Player import Player, config_players
+from common.colors import Color
+
+COLOR = Color()
 
 if __name__=="__main__":
     print("Welcome to RoyalArgmaPartyBros")
@@ -8,7 +11,8 @@ if __name__=="__main__":
 
         # init pygame
     pygame.init()
-    size = (800, 500)
+    size = (420, 250)
+    background = pygame.image.load("img/boards/board1.png")
     # crear ventana
     screen = pygame.display.set_mode(size)
 
@@ -16,5 +20,15 @@ if __name__=="__main__":
     close = False
     while not close:
         for event in pygame.event.get():
+            print(event)
             if event.type == pygame.QUIT:
                 close = True
+        screen.fill(COLOR.WHITE)
+        screen.blit(background, (0,0))
+
+        # --- Draw Zone Init ---
+
+        # --- Draw Zone End ---
+
+        # update screen
+        pygame.display.flip()
