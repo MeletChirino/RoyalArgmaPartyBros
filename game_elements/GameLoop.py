faea0225fbe4be2fe2fd1caf521f0a49f5b9d266
Melet Chirino Caicedo <melet.chirino@astc-design.com>
2023-01-20 15:49:47 +0100

    Gameloop refactored to be more modular

--- game_elements/GameLoop.py
+++ game_elements/GameLoop.py
@@ -17,6 +17,7 @@ class GameLoop:
         self.save_kwargs(self.verbose, 'verbose', kwargs)
         self.description = ""
         self.save_kwargs(self.description, 'description', kwargs)
+        self.init_time = 0
 
         self.trans_info = {
             # transitions info
@@ -47,23 +48,23 @@ class GameLoop:
         }
 
         self.run_info = {
-            START_TURN: self.start_turn,
+            START_TURN: self.start_turn, # 0
             # Presentacion muestra el nombre del jugador (notifica al server)
-            PRESENTATION: self.presentation,
+            PRESENTATION: self.presentation,# 1
             # Aqui es por si el jugador quiere escoger un item que tenga
-            CHOOSE_ITEM: self.item_event,
+            CHOOSE_ITEM: self.choose_item,# 2
             # Aqui espera a que el jugador escoja un Item
-            ITEM_EVENT: None, # Todavia no tengo ni la menor idea de que hacer aqui
+            ITEM_EVENT: self.item_event, # 3 Todavia no tengo ni la menor idea de que hacer aqui
             # Este es el espacio donde el item hace efecto
-            ROLL_DICE: self.roll_dice,
+            ROLL_DICE: self.roll_dice,# 4
             # Tiras el dado
-            MOVEMENT: self.movement,
+            MOVEMENT: self.movement,# 5
             # Se mueve
-            SQUARE_EVENT: self.square_event,
+            SQUARE_EVENT: self.square_event,# 6
             # Si en la casilla hay algun evento, pasa aqui
-            NEXT_PLAYER: self.next_player,
+            NEXT_PLAYER: self.next_player,# 7
             # Estado que da paso al sgte jugador, si lo hay
-            GAME_EVENT: self.game_event,
+            GAME_EVENT: self.game_event,# 8
             # Aqui pasan los minijuegos
         }
 
@@ -83,11 +84,6 @@ class GameLoop:
         print(F"Max turns = {self.max_turns}")
 
         self.clock = pygame.time.Clock()
-    
-    def save_kwargs(self, val, key, kwargs):
-        if key in kwargs.keys():
-            val = kwargs[key]
-            print(f"{key}: {val} saved.")
 
     def start(self):
         self.screen = pygame.display.set_mode(self.win_size)
@@ -103,13 +99,112 @@ class GameLoop:
         st_function()
 
         # --- Draw Board init
-        self.board.draw(self.screen, self.offset)
-        self.draw_stats()
-        self.draw_players()
     
         # update screen
         pygame.display.update()
         self.clock.tick(1) # Frames Per Second
+
+    # === State 0 ===
+    def start_turn(self):
+        # Here you should save some kind of timer value so that next states lasts for a few secons
+        self.screen.fill(WHITE)
+        self.init_time = pygame.time.get_ticks()
+        self.current_state = PRESENTATION
+
+    # === State 1 ===
+    def presentation(self):
+        # Drawing Rectangle
+        self.screen.fill(WHITE)
+        rect = pygame.Rect(
+            self.board.properties["presentation_rect"][0], # x coord
+            self.board.properties["presentation_rect"][1], # y coord
+            self.board.properties["presentation_rect_w"], # width
+            self.board.properties["presentation_rect_h"], # height
+        )
+        # TODO: Replace this rectangle with a beautiful draw
+        pygame.draw.rect(self.screen, GRAY, rect)
+
+        title_font = pygame.font.SysFont(
+            self.board.properties["presentation_font"],
+            self.board.properties["presentation_font_size"]
+            )
+        player = self.players[self.turn]
+        label = title_font.render(F"{player.name}'s turn", 1, BLACK)
+        self.screen.blit(
+            label,
+            self.board.properties["presentation_label_coord"]
+            )
+        pygame.display.update([rect])
+        current_time = pygame.time.get_ticks() - self.init_time
+        self.clock.tick(1) # Frames Per Second
+        if (current_time >= 3000):
+            print("timeout")
+            WAIT_5S.happen()
+
+    # === State 2 ===
+    def choose_item(self):
+        print("ITEM event happening")
+        self.game_draw()
+        ITEM_NOT_CHOOSEN.happen()
+
+    # === State 3 ===
+    def item_event(self):
+        pass
+
+    # === State 4 ===
+    def roll_dice(self):
+        self.game_draw()
+        self.dice_n = roll_dice()
+        print(F"DICE = {self.dice_n}")
+        DICE_ROLLED.happen()
+
+    # === State 5 ===
+    def movement(self):
+        if(self.dice_n == 0):
+            SQUARE_EVENT_BEGINGS.happen()
+            #MOVE_FINISHED.happen()
+            return
+
+        player = self.players[self.turn]
+        x = player.pos.x
+        y = player.pos.y
+        dir = self.board.properties["square_dir"][y][x]
+        player.advance(dir)
+        self.dice_n -= 1
+        self.game_draw()
+
+    # === State 6 ===
+    def square_event(self):
+        self.game_draw()
+        # Maybe you should draw somthing else here, depending on the event
+        player = self.players[self.turn]
+        x = player.pos.x
+        y = player.pos.y
+        ev = self.board.properties["square_ev"][y][x]
+        print(f"{player} event {ev}")
+        ev(player)
+        SQUARE_EVENT_FINISHED.happen()
+
+    # === State 7 ===
+    def next_player(self):
+        self.turn += 1
+        if self.turn >= self.max_turns:
+            self.turn = 0
+            self.current_state = GAME_EVENT
+        else:
+            self.current_state = PRESENTATION
+
+    # === State 8 ===
+    def game_event(self):
+        # Mini Game event, present the game and wait for results. Then pass the turn to next player
+        print("Play jenga")
+        self.game_draw()
+        # You should draw something different, depending on the event
+        TURN_FINISHED.happen()
+
+    # === Not relevant methods ===
+    def attach_event(self, event):
+        event.attach(self)
 
     def draw_stats(self):
         # Reorder players
@@ -140,65 +235,11 @@ class GameLoop:
             if (player.game_position == position):
                 return player
 
-    def square_event(self):
-        player = self.players[self.turn]
-        x = player.pos.x
-        y = player.pos.y
-        ev = self.board.properties["square_ev"][y][x]
-        print(f"{player} event {ev}")
-        ev(player)
-        SQUARE_EVENT_FINISHED.happen()
-
-    def movement(self):
-        if(self.dice_n == 0):
-            SQUARE_EVENT_BEGINGS.happen()
-            #MOVE_FINISHED.happen()
-            return
-
-        player = self.players[self.turn]
-        x = player.pos.x
-        y = player.pos.y
-        dir = self.board.properties["square_dir"][y][x]
-        player.advance(dir)
-        self.dice_n -= 1
-
     def transition(self, event):
         transitions = self.trans_info[self.current_state]
         for transition in transitions:
             if transition.match_ev(event):
                 self.current_state = transition.next_st()
-
-    def presentation(self):
-        # Drawing Rectangle
-        timeout = False
-        init_time = pygame.time.get_ticks()
-        while(not timeout):
-            rect = pygame.Rect(
-                self.board.properties["presentation_rect"][0], # x coord
-                self.board.properties["presentation_rect"][1], # y coord
-                self.board.properties["presentation_rect_w"], # width
-                self.board.properties["presentation_rect_h"], # height
-            )
-            # TODO: Replace this rectangle with a beautiful draw
-            pygame.draw.rect(self.screen, GRAY, rect)
-
-            title_font = pygame.font.SysFont(
-                self.board.properties["presentation_font"],
-                self.board.properties["presentation_font_size"]
-                )
-            player = self.players[self.turn]
-            label = title_font.render(F"{player.name}'s turn", 1, BLACK)
-            self.screen.blit(
-                label,
-                self.board.properties["presentation_label_coord"]
-                )
-            pygame.display.update([rect])
-            current_time = pygame.time.get_ticks() - init_time
-            self.clock.tick(1) # Frames Per Second
-            if (current_time >= 3000):
-                print("timeout")
-                WAIT_5S.happen()
-                timeout = True
 
     def show_avatars(self):
         self.screen.fill(WHITE)
@@ -257,43 +298,12 @@ class GameLoop:
                     print("Character not eligible")
                     choosen = False
 
-    def roll_dice(self):
-        # --- Get events init
-        print(F"Rolling dice")
-        self.dice_n = roll_dice()
-        print(F"DICE = {self.dice_n}")
-        DICE_ROLLED.happen()
-        for event in pygame.event.get():
-            print(event)
-            if event.type == pygame.KEYDOWN:
-                # for more info https://www.pygame.org/docs/ref/key.html
-                if event.key == pygame.K_SPACE:
-                    pass
-                    #roll dice
+    def save_kwargs(self, val, key, kwargs):
+        if key in kwargs.keys():
+            val = kwargs[key]
+            print(f"{key}: {val} saved.")
 
-        # mouse_pos = pygame.mouse.get_pos()
-        # --- Get events end
-        # Draw a rolling dice for a few seconds
-
-    def start_turn(self):
-        # Here you should save some kind of timer value so that next states lasts for a few secons
-        self.current_state = PRESENTATION
-
-    def next_player(self):
-        self.turn += 1
-        if self.turn >= self.max_turns:
-            self.turn = 0
-            self.current_state = GAME_EVENT
-        else:
-            self.current_state = PRESENTATION
-
-    def game_event(self):
-        # Mini Game event, present the game and wait for results. Then pass the turn to next player
-        print("Play jenga")
-        TURN_FINISHED.happen()
-    
-    def item_event(self):
-        ITEM_NOT_CHOOSEN.happen()
-
-    def attach_event(self, event):
-        event.attach(self)
+    def game_draw(self):
+        self.board.draw(self.screen, self.offset)
+        self.draw_stats()
+        self.draw_players()
